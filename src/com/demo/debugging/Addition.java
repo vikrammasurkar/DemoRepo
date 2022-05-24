dac3726569ef61df46b204edbc4bf1eadd1ff5b9
vikrammasurkar <vikrammasurkar007@gmail.com>
2022-05-24 02:51:16 -0700

    Added two files

---
+++ src/com/demo/debugging/Addition.java
@@ -0,0 +1,10 @@
+package com.demo.debugging;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+	}
+
+}
---
+++ src/com/demo/debugging/Subtraction.java
@@ -0,0 +1,10 @@
+package com.demo.debugging;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Substract");
+	}
+
+}
